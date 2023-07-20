5892f49b1c3e5be1e34f4fba41a2dcebcd3d3f40
chiwenc <adam07154211@gmail.com>
2023-07-20 16:41:57 +0800

    remove models.py file

--- Week-4/Assignment-3/models.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-
-connect_db = pymysql.connect(host = 'localhost', 
-                            port=3306, 
-                            user='Adam', 
-                            passwd='1019', 
-                            charset='utf8', 
-                            db='assignment')
-
