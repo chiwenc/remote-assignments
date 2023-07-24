117443d62305b12c43b0e0cb2fa75bec345e5f26
chiwenc <adam07154211@gmail.com>
2023-07-24 09:57:00 +0800

    adjust query for Q2

--- Week-4/Assignment-4/w4_4.sql
+++ Week-4/Assignment-4/w4_4.sql
@@ -3,6 +3,8 @@ SELECT article, email FROM assignment.article a
 LEFT JOIN assignment.user u ON a.user_id = u.id
 
 -- Q2: select articles from 7th to 12th sorted by id.
-SELECT id, article FROM assignment.article
-WHERE id BETWEEN 7 AND 12
-ORDER BY id
+SELECT id, article FROM (
+	SELECT * 
+    ,ROW_NUMBER() OVER (ORDER BY ID) row_num
+    FROM assignment.article) art
+WHERE row_num BETWEEN 7 AND 12
